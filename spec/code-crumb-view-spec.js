572d285217d4b9553f5f953fc70c797a877eb95d
Alexander Sanchez <asanchez.dev@gmail.com>
2017-10-15 15:47:53 -0400

    I have a little something. need to hook up the wires

---
+++ spec/code-crumb-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeCrumbView from '../lib/code-crumb-view';
+
+describe('CodeCrumbView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
